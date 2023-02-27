fa4356a7e11a1c74494da8abd02d3d0cf7e1a2b9
Quinn Webber <webber.quinn@gmail.com>
2023-02-27 16:13:18 -0500

    Update 01-clean-data.R

--- code/01-clean-data.R
+++ code/01-clean-data.R
@@ -43,4 +43,41 @@ df16$id[df16$id == "FO2016018"] <- ""
 df16$id[df16$id == "FO2016019"] <- ""
 df16$id[df16$id == "FO2016020"] <- ""
 
+#### calculate density estimate per day #### 
 
+## remove calves from group size count 
+df16$group_size <- df16$group_size - df16$calf
+
+## new column for presence/absence of collar
+df16[, boolean := id == "FO2016010" | id == "FO2016008" |
+       id == "FO2016001" | id == "FO2016012" |
+       id == "FO2016014" | id == "FO2016015" | 
+       id == "FO2016004" | id == "FO2016005" |
+       id == "FO2016003" | id == "FO2016011" |
+       id == "FO2016002" | id == "FO2016009"]
+
+## convert boolean to 1/0
+df16$boolean[df16$boolean == "TRUE"] <- 1
+df16$boolean[df16$boolean == "FALSE"] <- 0
+
+## calculate number of collars per group
+df16[, collar_group_size := sum(boolean), by = "rowid"]
+
+## new data file with group size and number of collars per day
+df16_sum <- df16[, sum(group_size), by = "doy"]
+df16_sum$collar_gs <- df16[, sum(collar_group_size), by = "doy"]$V1
+setnames(df16_sum, "V1", "gs")
+
+## total number of animals
+df16_sum$total <- 300
+
+## total number of collars
+df16_sum$all_collars <- 14
+
+## collars seen/caribou seen
+df16_sum$prop <- df16_sum$collar_gs/df16_sum$gs
+
+
+
+ggplot(df16_sum) +
+  geom_point(aes(doy, gs))
